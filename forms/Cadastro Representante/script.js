0b3fc96b8eadc23851fe0adb41d27044cd752707
NicolasSendeski <nicolas_sendeski@hotmail.com>
2020-03-19 13:10:48 -0300

    Desenvolvimento -> tabs empresa e outros dados

--- forms/Cadastro Representante/script.js
+++ forms/Cadastro Representante/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
- $('#cep').focusout(function () {
-        var cep = $('#cep').val();
+ $('#representante_cep').focusout(function () {
+        var cep = $('#representante_cep').val();
         cep = cep.replace('-','');
 
         var urlstr = 'http://viacep.com.br/ws/'+cep+'/json/';
@@ -10,10 +10,31 @@ $(document).ready(function () {
             dataType :'json',
             success : function (data) {
                 console.log(data);
-                $('#cidade').val(data.localidade);
-                $('#rua').val(data.logradouro);
-                $('#uf').val(data.uf);
-                $('#bairro').val(data.bairro);
+                $('#representante_cidade').val(data.localidade);
+                $('#representante_rua').val(data.logradouro);
+                $('#representante_uf').val(data.uf);
+                $('#representante_bairro').val(data.bairro);
+
+            },
+            error : function (erro) {
+                console.log(erro)
+            }
+        });
+    });$('#empresa_cep').focusout(function () {
+        var cep = $('#empresa_cep').val();
+        cep = cep.replace('-','');
+
+        var urlstr = 'http://viacep.com.br/ws/'+cep+'/json/';
+        $.ajax({
+            url : urlstr,
+            type :'get',
+            dataType :'json',
+            success : function (data) {
+                console.log(data);
+                $('#empresa_cidade').val(data.localidade);
+                $('#empresa_rua').val(data.logradouro);
+                $('#empresa_uf').val(data.uf);
+                $('#empresa_bairro').val(data.bairro);
 
             },
             error : function (erro) {
